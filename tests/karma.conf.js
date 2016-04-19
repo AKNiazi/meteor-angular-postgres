0921a381f1f23b0eef6169123f849145280d4e53
abdullah <abdullah.khan.niazi@hotmail.com>
2016-04-19 11:34:13 +0500

    added the missing files

---
+++ tests/karma.conf.js
@@ -0,0 +1,45 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      // paths loaded by Karma
+      {pattern: '../node_modules/systemjs/dist/system-polyfills.js', included: true, watched: true},
+      {pattern: '../node_modules/systemjs/dist/system.src.js', included: true, watched: true},
+      {pattern: '../node_modules/es6-shim/es6-shim.js', included: true, watched: true},
+      {pattern: '../node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
+      {pattern: '../node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
+      {pattern: '../node_modules/angular2/bundles/angular2.js', included: true, watched: true},
+      {pattern: '../node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
+
+      {pattern: 'karma-test-shim.js', included: true, watched: true},
+      //{pattern: 'src/test/matchers.js', included: true, watched: true},
+
+      // paths loaded via module imports
+      {pattern: '../client/**/*.js', included: false, watched: true},
+      {pattern: 'client/**/*.js', included: false, watched: true},
+      {pattern: 'server/**/*.js', included: false, watched: true},
+      // paths loaded via Angular's component compiler
+      // (these paths need to be rewritten, see proxies section)
+      //{pattern: 'src/**/*.html', included: false, watched: true},
+      //{pattern: 'src/**/*.css', included: false, watched: true},
+
+      // paths to support debugging with source maps in dev tools
+      //{pattern: 'client/**/*.ts', included: false, watched: false},
+      //{pattern: 'test/**/*.ts', included: false, watched: false},
+      //{pattern: 'client/**/*.js.map', included: false, watched: false},
+      //{pattern: 'test/**/*.js.map', included: false, watched: false}
+    ],
+
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  })
+}
